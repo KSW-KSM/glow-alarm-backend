e863cbb0f856120c7dbcefa681b368957ede6c92
Brian <bwook9908@gmail.com>
2024-01-22 15:09:08 +0900

    feat: swagger implemented(actually was implemented already, and I add a bit)

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 from models.create_table import init_table
 from routers import users, posts
 
-app = FastAPI()
+app = app = FastAPI(
+    title="boilerplate api",
+    description="This is a sample FastAPI application with Swagger documentation.",
+    version="1.0.0",
+    openapi_url="/openapi.json",  # JSON 형식의 API 스키마
+)
 
 # Initialize table
 init_table()
