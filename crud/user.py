1da054cc4827e910992761a81a3042cb6ecf2940
Brian <bwook9908@gmail.com>
2024-02-08 13:06:28 +0900

    feat: user api make

---
+++ crud/user.py
@@ -0,0 +1,51 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import select, update, delete
+from models.user import User
+from datetime import datetime
+
+
+class CRUDUser:
+    @staticmethod
+    def insert(db: Session, *, id: str, user_name: str, google_id: str, guardian_contact: str, bulb_ip: str, value: int):
+        user = User(id=id, user_name=user_name, google_id=google_id, guardian_contact=guardian_contact, bulb_ip=bulb_ip, value=value)
+        try:
+            db.add(user)
+            db.commit()
+            db.refresh(user)
+        except IntegrityError:
+            db.rollback()
+            raise ValueError("User with ID already exists.")
+        return user
+
+    @staticmethod
+    def get(db: Session, id: str):
+        return db.get(User, id)
+
+    @staticmethod
+    def get_all(db: Session):
+        return db.query(User).all()
+
+    @staticmethod
+    def update(db: Session, *, id: str, user_name: str, google_id: str, guardian_contact: str, bulb_connection: bool, bulb_ip: str, value: int):
+        # Update the user
+        updated_user = db.get(User, id)
+        if updated_user:
+            updated_at = datetime.now()
+            db.query(User).filter(User.id == id).update({"user_name": user_name, "google_id": google_id, "guardian_contact": guardian_contact, "bulb_connection": bulb_connection, "bulb_ip": bulb_ip, "value": value, "updated_at": updated_at})
+            db.commit()
+            db.refresh(updated_user)
+
+        return updated_user
+
+    @staticmethod
+    def delete(db: Session, id: str):
+        deleted_user = db.get(User, id)
+        if deleted_user:
+            db.delete(deleted_user)
+            db.commit()
+        return deleted_user
+
+
+# 싱글톤 객체 생성
+crud_user = CRUDUser()
