a39ddf99f582b7d3564db47c5ab38e56ee02bfcd
Brian <bwook9908@gmail.com>
2024-02-10 19:15:55 +0900

    fix: enum type import change

--- models/alarm.py
+++ models/alarm.py
@@ -1,7 +1,6 @@
 from db.base import Base
 from models.columns.timestamp import TimeStampedModel
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
-from sqlalchemy_enum import EnumType
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from enum import Enum
@@ -22,8 +21,8 @@ class Alarm(Base, TimeStampedModel):
 
     id = Column(String(20), primary_key=True, index=True)
     alarm_time = Column(DateTime(timezone=True), nullable=False, default=datetime.utcnow)
-    repeat_day = Column(EnumType(WeekdayEnum, create_constraint=False), nullable=True)
-    light_color= Column(EnumType(LightColor, create_constraint=False), nullable = False, default = LightColor.RED)
+    repeat_day = Column(Enum(WeekdayEnum, create_constraint=False), nullable=True)
+    light_color= Column(Enum(LightColor, create_constraint=False), nullable = False, default = LightColor.RED)
     alarm_status = Column(Column(Boolean, nullable=False, default=False))
 
     user_id = Column(String(20), ForeignKey('user.id'))
