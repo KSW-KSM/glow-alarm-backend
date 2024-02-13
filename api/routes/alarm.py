982af7c2b22239c21790c465df65db699faf7739
Brian <bwook9908@gmail.com>
2024-02-13 17:13:47 +0900

    fix: alarm response, request change
    - now pk id auto generates with uuid

--- api/routes/alarm.py
+++ api/routes/alarm.py
@@ -5,20 +5,20 @@ from typing import List
 from sqlalchemy.orm import Session
 from api.depends.get_db import get_db
 from crud.alarm import crud_alarm
-from schemas.alarm import AlarmCreate, AlarmUpdate, AlarmInDB
+from schemas.alarm import AlarmCreate, AlarmUpdate, AlarmInDB, AlarmResponse
 
 router = APIRouter()
 
-@router.post("/alarm", response_model=AlarmInDB)
+@router.post("/alarm", response_model=AlarmResponse)
 def create_alarm(alarm: AlarmCreate, db: Session = Depends(get_db)):
     try:
-        created_alarm = crud_alarm.insert(db=db, id=alarm.id, alarm_time=alarm.alarm_time, repeat_day=alarm.repeat_day, light_color=alarm.light_color, alarm_status=alarm.alarm_status, user_id=alarm.user_id)
+        created_alarm = crud_alarm.insert(db=db, alarm_time=alarm.alarm_time, repeat_day=alarm.repeat_day, light_color=alarm.light_color, alarm_status=alarm.alarm_status, user_id=alarm.user_id)
     except Exception as e:
         raise HTTPException(status_code=404, detail=f"Alarm creation failed: {str(e)}")
 
     return JSONResponse(status_code=201, content=jsonable_encoder(created_alarm))
 
-@router.get("/alarm/{id}", response_model=AlarmInDB)
+@router.get("/alarm/{id}", response_model=AlarmResponse)
 def read_alarm(id: str = Path(..., min_length=1), db: Session = Depends(get_db)):
     try:
         alarm = crud_alarm.get(db, id)
@@ -28,7 +28,7 @@ def read_alarm(id: str = Path(..., min_length=1), db: Session = Depends(get_db))
         raise HTTPException(status_code=404, detail="Alarm not found")
     return JSONResponse(status_code=200, content=jsonable_encoder(alarm))
 
-@router.get("/alarms/user/{user_id}", response_model=List[AlarmInDB])
+@router.get("/alarms/user/{user_id}", response_model=List[AlarmResponse])
 def read_alarms_by_user_id(user_id: str = Path(..., min_length=1), db: Session = Depends(get_db)):
     alarms = crud_alarm.get_all_by_user_id(db, user_id)
     return JSONResponse(status_code=200, content=jsonable_encoder(alarms))
@@ -38,7 +38,7 @@ def read_alarms(db: Session = Depends(get_db)):
     alarms = crud_alarm.get_all(db)
     return JSONResponse(status_code=200, content=jsonable_encoder(alarms))
 
-@router.put("/alarm/{id}", response_model=AlarmInDB)
+@router.put("/alarm/{id}", response_model=AlarmResponse)
 def update_alarm(id: str = Path(..., min_length=1), db: Session = Depends(get_db),
                  alarm: AlarmUpdate = Body(...)):
     try:
