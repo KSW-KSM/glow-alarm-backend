5071ddcaf38d7395bfdd2936d8985aa3960e93b3
clice lee <131771046+clicelee@users.noreply.github.com>
2024-02-13 14:32:36 +0900

    feat: add bulb control function
    -patch: core.bulb_controll.py
    -add: def disaster_function_high_priority(bulb)
    -add: def disaster_function_medium_priority(bulb)
    -add: personal_alarm(bulb, selected_color)

--- core/bulb_controll.py
+++ core/bulb_controll.py
@@ -7,47 +7,45 @@ import time
 # 주기적으로 깜빡거리는 함수 만들기
 
 
-print(discover_bulbs())
+# 재난함수(중요도 상)
+def disaster_function_high_priority(bulb):
+    while True:
+        bulb.set_rgb(255, 0, 0)  # 빨간색으로 설정
+        bulb.set_brightness(50)  # 밝기를 50%로 설정
+        time.sleep(15)  # 15초 대기
+        bulb.turn_off()  # 5초 꺼짐
+        time.sleep(5)  
+        bulb.set_brightness(70)  # 밝기를 70%로 설정
+        time.sleep(15)  # 15초 대기
+        bulb.turn_off()  # 5초 꺼짐
+        time.sleep(5)  
+        bulb.set_brightness(100)  # 밝기를 100%로 설정
+        time.sleep(15)  # 15초 대기
+        bulb.turn_off()  # 5초 꺼짐
+        time.sleep(5) 
 
+# 재난함수(중요도 중)
+def disaster_function_medium_priority(bulb):
+    while True:
+        bulb.set_rgb(0, 0, 255)  # 파란색으로 설정
+        bulb.set_brightness(10)  # 밝기를 10%로 설정
+        time.sleep(10)  # 10초 대기
+        bulb.turn_off()  # 5초 꺼짐
+        time.sleep(5)  
+        bulb.set_brightness(30)  # 밝기를 30%로 설정
+        time.sleep(10)  # 10초 대기
+        bulb.turn_off()  # 5초 꺼짐
+        time.sleep(5)  
+        bulb.set_brightness(50)  # 밝기를 50%로 설정
+        time.sleep(10)  # 10초 대기
+        bulb.turn_off()  # 최종꺼짐
 
-bulb = Bulb('172.30.1.64')
-
-
-def blink_periodically(bulb, interval_seconds, total_blinks):
-    for _ in range(total_blinks):
-        bulb.toggle()  # 불 켜기/끄기
-        time.sleep(interval_seconds)  # 주기적으로 깜빡이기 위한 대기
-
-
-def control_bulb(bulb, action, *args):
-    if action == 'turn_on':
-        bulb.turn_on()
-    elif action == 'turn_off':
-        bulb.turn_off()
-    elif action == 'toggle':
-        bulb.toggle()
-    elif action == 'set_brightness':
-        if len(args) == 1:
-            bulb.set_brightness(args[0])
-        elif len(args) == 2:
-            bulb.set_brightness(args[0], light_type=LightType.Ambient)
-    elif action == 'set_rgb':
-        if len(args) == 3:
-            bulb.set_rgb(*args)
-    elif action == 'set_hsv':
-        if len(args) == 2:
-            bulb.set_hsv(args[0], args[1])
-        elif len(args) == 3:
-            bulb.set_hsv(args[0], args[1], args[2])
-    elif action == 'set_color_temp':
-        if len(args) == 1:
-            bulb.set_color_temp(args[0])
-    elif action == 'set_default':
-        bulb.set_default()
-    else:
-        print("Invalid action")
-
-
-# control_bulb(bulb, 'set_brightness' ,100)
-# control_bulb(bulb, 'turn_on')
-blink_periodically(bulb, 0.5, 10)
+# 개인 알람
+def personal_alarm(bulb, selected_color):
+    while True:
+        # 어플에서 선택한 색상을 설정
+        bulb.set_rgb(selected_color[0], selected_color[1], selected_color[2])
+        bulb.turn_on()  # 전구 켜기
+        time.sleep(4)  # 4초 대기
+        bulb.turn_off()  # 전구 끄기
+        time.sleep(1)  # 1초 대기
