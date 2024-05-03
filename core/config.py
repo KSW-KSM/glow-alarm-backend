b1b94d105a5e34568f5968e9e7108b428a1eb9c8
Brian <bwook9908@gmail.com>
2024-05-03 17:44:58 +0900

    fix: change db url

--- core/config.py
+++ core/config.py
@@ -1,11 +1,18 @@
 from typing import Any, Dict, List, Optional, Union
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 
 from pydantic import AnyHttpUrl, validator
 from sqlalchemy.engine import create_engine
 
+load_dotenv()
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra="allow")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
+    db_url = os.getenv("DATABASE_URL")
     
 settings = Settings()
